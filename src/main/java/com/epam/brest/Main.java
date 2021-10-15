657614b146334facf975646da36d350b4f036820
Yauheni Sivenka <e.sivenko2013@gmail.com>
2021-10-15 14:55:53 +0300

    Read from file

--- src/main/java/com/epam/brest/Main.java
+++ src/main/java/com/epam/brest/Main.java
@@ -2,6 +2,10 @@ package com.epam.brest;
 
 import com.epam.brest.calc.CalcImpl;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
@@ -9,28 +13,71 @@ public class Main {
 
     public static void main(String[] args) {
         BigDecimal weight, pricePerKg, distance, pricePerKm;
+        boolean res, inputFromFile;
+        String charInputFromFile;
+
         try (Scanner in = new Scanner(System.in)) {
-        //    while (!in.hasNext("q")) {
+            System.out.println("Are you want take a prices from file? (y/n)");
+            charInputFromFile = in.next();
+            inputFromFile = (charInputFromFile.equals("y"));
             do{
+                System.out.println("Input any char for exit:");
                 weight = getValueFromCon(in, "Input weight: ");
-                pricePerKg = getValueFromCon(in, "Input price/kg : ");
                 distance = getValueFromCon(in, "Input distance: ");
-                pricePerKm = getValueFromCon(in, "Input price/km: ");
-                System.out.println("Weight = "+weight+"; distance = "+distance+"; price per kg = "+pricePerKg+"; price per km = "+pricePerKm);
-                BigDecimal result = new CalcImpl().handle(weight,pricePerKg,distance,pricePerKm);
-                System.out.println("Result: "+result);
-                System.out.println("Input q for exit or c for continue");
-            } while (!in.hasNext("q"));
-            // TODO: сделать правильный ввод
-        // }
+                if (!inputFromFile) {
+                    pricePerKg = getValueFromCon(in, "Input price/kg : ");
+                    pricePerKm = getValueFromCon(in, "Input price/km: ");
+                } else {
+                    pricePerKg = getValueFromFile(weight,"/home/yauheni/Development/Project/Yauheni-Sivenka/resources/pricePerKg.csv");
+                    pricePerKm = getValueFromFile(distance,"/home/yauheni/Development/Project/Yauheni-Sivenka/resources/pricePerKm.csv");
+                }
+                res = !((weight.compareTo(BigDecimal.valueOf(0))<0) |
+                        (pricePerKg.compareTo(BigDecimal.valueOf(0))<0) |
+                        (distance.compareTo(BigDecimal.valueOf(0))<0) |
+                        (pricePerKm.compareTo(BigDecimal.valueOf(0))<0));
+                if (res) {
+                    System.out.println("Weight = " + weight + "; distance = " + distance + "; price per kg = " + pricePerKg + "; price per km = " + pricePerKm);
+                    BigDecimal result = new CalcImpl().handle(weight, pricePerKg, distance, pricePerKm);
+                    System.out.println("Result: " + result);
+                } else
+                    System.out.println("Bye!");
+            } while (res);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     private static BigDecimal getValueFromCon(Scanner scanner, String outputMessage) {
         BigDecimal value;
-        System.out.print(outputMessage);
-        value = scanner.nextBigDecimal();
+        try {
+            System.out.print(outputMessage);
+            value = scanner.nextBigDecimal();
+        }
+        catch (Exception e) {
+            value = BigDecimal.valueOf(-1);
+        }
         return value;
     }
+
+    private static BigDecimal  getValueFromFile(BigDecimal value, String fileName) throws IOException {
+        FileReader x = new FileReader(fileName);
+        BufferedReader varRead = new BufferedReader(x);
+        String oneLine;
+        String[] oneLineByWords;
+        BigDecimal minValue;
+        BigDecimal price = BigDecimal.valueOf(0);
+
+        while ((oneLine = varRead.readLine()) != null) {
+            oneLineByWords = oneLine.split(",");
+            minValue = BigDecimal.valueOf(Double.parseDouble(oneLineByWords[0]));
+            if (value.compareTo(minValue)<0) {
+                break;
+            }
+            price = BigDecimal.valueOf(Double.parseDouble(oneLineByWords[1]));
+        }
+        varRead.close();
+        return price;
+    }
+
 }
 
--- src/main/java/com/epam/brest/calc/CalcImpl.java
+++ src/main/java/com/epam/brest/calc/CalcImpl.java
@@ -2,7 +2,8 @@ package com.epam.brest.calc;
 
 import java.math.BigDecimal;
 
-public class CalcImpl implements Calc{
+public class CalcImpl implements Calc {
+
     @Override
     public BigDecimal handle(BigDecimal weight, BigDecimal pricePerKg, BigDecimal distance, BigDecimal pricePerKm) {
         return weight.multiply(pricePerKg).add(distance.multiply(pricePerKm));
